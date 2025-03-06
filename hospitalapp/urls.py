c85d97edf67c2df5b9f7f2d8585f668c628f0669
Bitok-2 <bitokenock38@gmail.com>
2025-03-06 14:27:54 +0300

    labimed

---
+++ hospitalapp/urls.py
@@ -0,0 +1,22 @@
+
+from django.contrib import admin
+from django.urls import path
+from hospitalapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', views.index, name='index'),
+    path('starter/', views.starter, name='starter'),
+    path('about/', views.about, name='about'),
+    path('service/', views.service, name='service'),
+    path('department/', views.department, name='department'),
+    path('doctor/', views.doctor, name='doctor'),
+    path('appointment/', views.appointment, name='appointment'),
+    path('contact/', views.contact, name='contact'),
+    path('show/', views.show, name='show'),
+    path('delete/<int:id>', views.delete, name='show'),
+    path('edit/<int:id>', views.edit, name='edit'),
+    path('', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+
+]
